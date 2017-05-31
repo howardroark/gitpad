d3b38a6dd1f087e14e6bf2286a9f662d875fa706
howardroark <howardroark@users.noreply.github.com>
2017-05-31 08:29:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 
 var app = express();
 
@@ -6,7 +7,19 @@ app.use(express.static('public'));
 
 app.get('/callback', function (req, res) {
   var code = req.query.code;
-  res.send(code);
+  var accessToken;
+  request.get(
+    'https://github.com/login/oauth/access_token', 
+    {
+      code: code,
+      client_id: process.env.CLIENT_ID,
+      client_secrent: process.env.CLIENT_SECRET
+    },
+    function (result) {
+      accessToken = result.access_token;
+      res.send(accessToken);
+    }
+  );
 });
 
 app.listen(process.env.PORT || 3000);
